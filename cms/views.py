c11044086957a0f7b533510daa2e893bc98adcbb
arubiopa <anggylotis@gmail.com>
2016-04-12 20:14:58 +0200

    ejercicio hecho

---
+++ cms/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from models import Pages
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
+@csrf_exempt
+def cms(request, recurso):
+    if request.user.is_authenticated():
+        estado = "<br><br>Hola " + request.user.username +\
+                ". <a href='/admin/logout/'>Logout</a><br>"
+    else:
+        estado = "<br><br>No estas registrado. <a href='/admin/login/'>Login</a><br>"
+    if request.method == "GET":
+        try:
+    		contenido = Pages.objects.get(name=recurso)
+    		return HttpResponse(contenido.name+ ':' + contenido.page)
+    	except Pages.DoesNotExist:
+    		return HttpResponse("Recurso no encontrado: " + recurso)
+
+    elif request.method == "PUT":
+        if request.user.is_authenticated():
+            pagina = Pages(name=recurso, page=request.body)
+            pagina.save()
+            return HttpResponse("Pagina guardada: "+ request.body)
+        else:
+            return HttpResponse("no se puede añadir la pagina")
+    else:
+        return HttpResponse( "metodo no disponible")
+    return HttpResponse(estado)
